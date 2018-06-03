b5e7423cec129acb1c73b1e08418b192b0361301
Shefali-Sharma <shefali92@gmail.com>
2018-06-03 23:44:51 +0530

    All arguments passed
    See All arguments passed using console.log

--- NotesNode/app.js
+++ NotesNode/app.js
@@ -1,6 +1,8 @@
 console.log('Starting app.js');
 
 const fs = require('fs');
-const os = require('os');
 const _ = require('lodash');
+
 const notes = require('./notes.js');
+
+console.log(process.argv);
