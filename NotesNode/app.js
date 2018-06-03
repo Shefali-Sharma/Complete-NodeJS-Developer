404af5f64e525db474a8838294c90c5c67312540
Shefali-Sharma <shefali92@gmail.com>
2018-06-03 23:40:29 +0530

    Removing Old Code
    Removing Old Code

--- NotesNode/app.js
+++ NotesNode/app.js
@@ -4,25 +4,3 @@ const fs = require('fs');
 const os = require('os');
 const _ = require('lodash');
 const notes = require('./notes.js');
-
-//var user = os.userInfo();
-// console.log(user);
-
-//fs.appendFile('greeting.txt', 'Hello World!', function(err){
-//  if(err){
-//    console.log('Unable to write to file.');
-//  }
-//});
-
-//fs.appendFileSync('greeting.txt', 'Hello ' + user.username + '!');
-
-//fs.appendFileSync('greeting.txt', `Hello ${user.username} ! You are ${notes.age}.`);
-
-//console.log('Result:', notes.add(9,-2));
-
-//console.log(_.isString(true));
-//console.log(_.isString('Shefali'));
-
-var filteredArray = _.uniq(['Sahil', 1, 'Shefali', 1, 2, 3, 4]);
-
-console.log(filteredArray);
