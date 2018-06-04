7dd22c2f3053b2e9a22f1e4b5df710446de698ae
Shefali-Sharma <shefali92@gmail.com>
2018-06-04 09:20:05 +0530

    Adding Functions
    Adding Function definition outline in notes.js and calling them from app.js

--- NotesNode/app.js
+++ NotesNode/app.js
@@ -2,23 +2,27 @@ console.log('Starting app.js');
 
 const fs = require('fs');
 const _ = require('lodash');
-
+const yargs = require('yargs');
 const notes = require('./notes.js');
 
+const argv = yargs.argv;
 var command = process.argv[2];
 console.log('Command:', command);
+console.log('Process:', process.argv);
+console.log('Yargs:', argv);
+
 
 if(command === 'add'){
-  console.log('Adding a new Note');
+  notes.addNote(argv.title, argv.body);
 }
 else if(command === 'list'){
-  console.log('Listing all Notes');
+  notes.getAll();
 }
 else if(command === 'read'){
-  console.log('Reading Note');
+  notes.readNote();
 }
 else if(command === 'remove'){
-  console.log('Removing a Note');
+  notes.removeNote();
 }
 else{
   console.log('Command not recognized');
--- NotesNode/notes.js
+++ NotesNode/notes.js
@@ -1,5 +1,24 @@
 console.log('Starting Notes.');
 
-// console.log(module);
+var addNote = (title, body) => {
+  console.log('Adding Note', title, body);
+};
 
-module.exports.age = 25;
+var getAll = () => {
+  console.log('Getting all Notes');
+};
+
+var readNote = () => {
+  console.log('Reading Note');
+};
+
+var removeNote = () => {
+  console.log('Removing Note');
+};
+
+module.exports = {
+  addNote: addNote, //Or we can simply just write addNote - both are equivalent
+  getAll, //Or we could have written getAll : getAll
+  readNote,
+  removeNote
+};
