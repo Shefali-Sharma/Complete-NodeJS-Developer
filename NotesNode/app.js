626ab2f8b6d2cea4a1c84ed02f1c432f5a8ce8da
Shefali-Sharma <shefali92@gmail.com>
2018-06-04 08:45:46 +0530

    Installing Yargs
    Installing Yargs

--- NotesNode/app.js
+++ NotesNode/app.js
@@ -5,4 +5,21 @@ const _ = require('lodash');
 
 const notes = require('./notes.js');
 
-console.log(process.argv);
+var command = process.argv[2];
+console.log('Command:', command);
+
+if(command === 'add'){
+  console.log('Adding a new Note');
+}
+else if(command === 'list'){
+  console.log('Listing all Notes');
+}
+else if(command === 'read'){
+  console.log('Reading Note');
+}
+else if(command === 'remove'){
+  console.log('Removing a Note');
+}
+else{
+  console.log('Command not recognized');
+}
