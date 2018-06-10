5206f74d3033397b7c9a4ff185ab59ab9dbc718e
Shefali-Sharma <shefali92@gmail.com>
2018-06-10 09:37:03 +0530

    Refactoring Code for Reusability
    Creating fetchNotes and saveNotes in notes.js, as well as updating if(command == 'add') in app.js

--- NotesNode/app.js
+++ NotesNode/app.js
@@ -12,7 +12,18 @@ console.log('Yargs:', argv);
 
 
 if(command === 'add'){
-  notes.addNote(argv.title, argv.body);
+  var note = notes.addNote(argv.title, argv.body);
+  if(note){
+    console.log('${note.title}');
+    console.log('Note created and added.');
+    console.log('--');
+    console.log("Title: " + note.title);
+    console.log("Body: " + note.body);
+  }
+  else{
+    console.log('Note title taken.');
+  }
+
 }
 else if(command === 'list'){
   notes.getAll();
