43f70bf1925b358d783ab9faccded1c3363401a1
Shefali-Sharma <shefali92@gmail.com>
2018-07-19 12:19:02 +0530

    Updating Async-Basics
    Updating Async-Basics

--- WeatherApp/playground/async-basics.js
+++ WeatherApp/playground/async-basics.js
@@ -1,12 +1,11 @@
 console.log('Starting app');
 
-
 setTimeout(() => {
   console.log('Inside of callback');
 }, 2000);
 
 setTimeout(() => {
   console.log('Second Timeout works');
-}, 1000);
+}, 0);
 
 console.log('Finishing up');
