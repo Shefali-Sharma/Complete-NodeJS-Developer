cb2a531c2340502854f09bd9f5292af3e0173a78
Shefali-Sharma <shefali92@gmail.com>
2018-07-24 19:51:04 +0530

    Pretty Print object
    Pretty Print object

--- WeatherApp/app.js
+++ WeatherApp/app.js
@@ -4,5 +4,5 @@ request({
   url: 'https://maps.google.com/maps/api/geocode/json?address=%201301%20lombard%20street%20philadelphia&key=AIzaSyBmRAAxtCv3MgOCDggrJGpjCcx-HV0VK7U',
   json: true
 }, (error, reponse, body) => {
-  console.log(body);
+  console.log(JSON.stringify(body, undefined, 2));
 });
