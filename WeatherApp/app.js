ca38b63308bd45eed8aa86322146b0955383536a
Shefali-Sharma <shefali92@gmail.com>
2018-09-04 17:57:46 -0400

    Create seperate file for geocode
    Create seperate file for geocode: geocode.js

--- WeatherApp/app.js
+++ WeatherApp/app.js
@@ -1,5 +1,5 @@
-const request = require('request');
 const yargs = require('yargs');
+const geocode = require('./geocode/geocode')
 
 const argv = yargs
   .options({
@@ -14,25 +14,4 @@ const argv = yargs
 .alias('help', 'h')
 .argv;
 
-// console.log(argv);
-var encodedAddress = encodeURIComponent(argv.a);
-// var encodedAddress = encodeURIComponent(argv['a']); -- Both will work
-// decodeURTComponent -- converts encoded string to decoded string
-
-request({
-  url: 'https://maps.google.com/maps/api/geocode/json?address=' + encodedAddress,
-  json: true
-}, (error, response, body) => {
-  // console.log(JSON.stringify(response, undefined, 2))
-  if(error){
-    console.log('Unable to connect to Google servers.');
-  }else if(body.status === 'ZERO_RESULTS'){
-    console.log('Unable to find that address');
-  }
-  else if(body.status === 'OK'){
-    console.log('Address:' + body.results[0].formatted_address);
-    console.log('Latitude:' + body.results[0].geometry.location.lat);
-    console.log('Longitude:' + body.results[0].geometry.location.lng);
-  }
-
-});
+geocode.geocodeAddress(argv.address);
