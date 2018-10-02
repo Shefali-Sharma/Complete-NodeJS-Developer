bf0ce86e0314e5c9e68df70e412fce74d1a2a76f
Shefali-Sharma <shefali92@gmail.com>
2018-10-02 12:45:50 -0400

    Using promises for weatherapp
    Using promises for weatherapp

--- WeatherApp/app-promise.js
+++ WeatherApp/app-promise.js
@@ -1,4 +1,5 @@
 const yargs = require('yargs');
+const axios = require('axios');
 
 const argv = yargs
   .options({
@@ -13,3 +14,27 @@ const argv = yargs
 .alias('help', 'h')
 .argv;
 
+var encodedAddress = encodeURIComponent(argv.address);
+var geocodeUrl = 'https://maps.googleapis.com/maps/api/geocode/json?address=' + encodedAddress + '&key=YOURKEY';
+
+axios.get(geocodeUrl).then((response) => {
+  if(response.data.status === 'ZERO_RESULTS') {
+    throw new Error ('Unable to find that address');
+  }
+  var lat = response.data.results[0].geometry.location.lat;
+  var lng = response.data.results[0].geometry.location.lng;
+  var weatherUrl = 'https://api.forecast.io/forecast/YOURKEY/' + lat +',' + lng;
+  console.log(response.data.results[0].formatted_address);
+  return axios.get(weatherUrl);
+}).then((response) => {
+  var temperature = response.data.currently.temperature;
+  var apparentTemperature = response.data.currently.apparentTemperature;
+  console.log('It is currently ' + temperature + '. It feels like ' + apparentTemperature);
+}).catch((e) => {
+  if(e.code === 'ENOTFOUND' || e.code === 'ECONNREFUSED') {
+    console.log ('Unable to connect to API servers');
+  }
+  else {
+    console.log(e.message);
+  }
+});
