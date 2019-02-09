3c1fdff22a7eccefc565b6c0e91edb374613317c
Shefali-Sharma <shefali92@gmail.com>
2019-02-09 14:58:18 -0500

    Updating key
    Updating key

--- WeatherApp/playground/promise-2.js
+++ WeatherApp/playground/promise-2.js
@@ -5,7 +5,7 @@ var geocodeAddress = (address) => {
     var encodedAddress = encodeURIComponent(address);
 
     request({
-      url: 'https://maps.googleapis.com/maps/api/geocode/json?address=' + encodedAddress + '&key=YOURKEY',
+      url: 'https://maps.googleapis.com/maps/api/geocode/json?address=' + encodedAddress + '&key=AIzaSyAPrWXmgiHZfesvXuvtc3NsuGj0GbuvOsw',
       json: true
     }, (error, response, body) => {
       if (error) {
