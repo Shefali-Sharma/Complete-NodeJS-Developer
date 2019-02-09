93292f14b07267c5c3a26e8f6cbff388a163df43
Shefali-Sharma <shefali92@gmail.com>
2019-02-09 14:58:33 -0500

    Updating key
    Updating key

--- WeatherApp/app-promise.js
+++ WeatherApp/app-promise.js
@@ -15,7 +15,7 @@ const argv = yargs
 .argv;
 
 var encodedAddress = encodeURIComponent(argv.address);
-var geocodeUrl = 'https://maps.googleapis.com/maps/api/geocode/json?address=' + encodedAddress + '&key=YOURKEY';
+var geocodeUrl = 'https://maps.googleapis.com/maps/api/geocode/json?address=' + encodedAddress + '&key=AIzaSyAPrWXmgiHZfesvXuvtc3NsuGj0GbuvOsw';
 
 axios.get(geocodeUrl).then((response) => {
   if(response.data.status === 'ZERO_RESULTS') {
@@ -23,7 +23,7 @@ axios.get(geocodeUrl).then((response) => {
   }
   var lat = response.data.results[0].geometry.location.lat;
   var lng = response.data.results[0].geometry.location.lng;
-  var weatherUrl = 'https://api.forecast.io/forecast/YOURKEY/' + lat +',' + lng;
+  var weatherUrl = 'https://api.forecast.io/forecast/83320cbc01784674946bb11881aebe27/' + lat +',' + lng;
   console.log(response.data.results[0].formatted_address);
   return axios.get(weatherUrl);
 }).then((response) => {
